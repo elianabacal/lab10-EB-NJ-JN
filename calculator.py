6cbe0ca423523f32e3f8cfae7a8758f3405d2bab
Eliana Bacal <elianabacal@Elianas-MacBook-Pro.local>
2025-04-09 11:06:50 -0400

    merged changes

--- calculator.py
+++ calculator.py
@@ -33,24 +33,7 @@ def square_root(a):
     return math.sqrt(a)
 def hypotenuse(a, b):
     return math.hypot(a, b)
-def add(a, b):
-    return a + b
 
-def subtract(a, b):
-    return a - b
-
-def multiply(a, b):
-    return a * b
-def divide(a, b):
-    if a == 0:
-        raise ZeroDivisionError
-    return a / b
-def logarithm(a,b):
-    if b < 0:
-        raise ValueError
-    return math.log(a,b)
-def exponent(a,b):
-    return a ** b
 
 
 

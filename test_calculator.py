21127736fa09a36b4aeae2968b0376523fa9cc08
njupalli <nehajupalli06@gmail.com>
2025-04-09 21:22:44 -0400

    Made changes to test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -8,10 +8,14 @@ from calculator import *
 class TestCalculator(unittest.TestCase):
     # Partner 2
     def test_add(self): # 3 assertions
-        pass
+        self.assertEqual(add(1,2), 3)
+        self.assertEqual(add(2,2), 4)
+        self.assertEqual(add(3,3), 6)
 
     def test_subtract(self): # 3 assertions
-        pass
+        self.assertEqual(subtract(1,2), 2)
+        self.assertEqual(subtract(2,2), 1)
+        self.assertEqual(subtract(3,2), 1)
     ##########################
 
     # Partner 1
@@ -24,17 +28,18 @@ class TestCalculator(unittest.TestCase):
 
     # Partner 2
     def test_divide_by_zero(self): # 1 assertion
-        # call division function inside, example:
-        # with self.assertRaises(<INSERT_ERROR_TYPE>):
+        self.assertRaises(ZeroDivisionError)
         #     div(0, 5)
-        pass
+
 
     def test_logarithm(self): # 3 assertions
-        pass
+        self.assertEqual(math.log(8,2), 0)
+        self.assertEqual(math.log(1,2), 0)
+        self.assertEqual(math.log(1,1), 1)
 
     def test_log_invalid_base(self): # 1 assertion
         # use same technique from test_divide_by_zero
-        pass
+        self.assertRaises(ValueError)
     ##########################
     
     # Partner 1
